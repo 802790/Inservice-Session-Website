b37c03f21a5761e40da000f338b19b7fdbe58bfe
802790 <43350751+802790@users.noreply.github.com>
2019-09-10 14:06:59 -0400

    Add files via upload

--- Teacher Inservice Sign Up/script.js
+++ Teacher Inservice Sign Up/script.js
@@ -12,8 +12,20 @@ function createCards(){
 let container = document.createElement('div')
 container.innerHTML = sessions.map(session => 
 `<div>
-<h3>${session.Title}</h3>
+<h2>${session.Title}</h2>
+
+<h3>${session["Area of Focus"]}</h3>
+
+<h4>${session["Grade"]}</h4>
+
 <p>${session['Description']}</p>
+
+<h4>Presenter${session['Presenter(s)'].split(' and ').length > 1 ? 's' : ''}: ${session['Presenter(s)']}</h4>
+
+<h5>${session['Intended Audience']}</h5>
+
+<h5>${session['Building']}</h5>
+<button>Sign Up</Button>
 </div>`
                                   ).join('')
 document.querySelector('article')
